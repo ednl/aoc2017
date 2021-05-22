6a9585ae2a6f761e74080f3cfdab748be097f0a0
Ewoud Dronkert <ewoud@dronkert.net>
2021-05-22 19:33:35 +0200

    day 6

--- p06.py
+++ p06.py
@@ -1,2 +1,35 @@
-a = list(map(int, [a.split() for a in open("input06.txt")][0]))
-print(len(a), sum(a))
+import numpy as np
+
+def state(a):
+    return "".join(list(map(chr, a + 32)))
+
+a = np.loadtxt("input06.txt", dtype=np.uint8)
+banks = a.size
+
+seen = set()
+hist = list()
+hash = state(a)
+count = 0
+
+while hash not in seen:
+    count += 1
+    seen.add(hash)
+    hist.append(hash)
+
+    val = np.amax(a)
+    key = np.argmax(a)
+    a[key] = 0
+    q, r = divmod(val, banks)
+
+    a += q
+    next = key - banks + 1
+    for i in range(next, next + r):
+        a[i] += 1
+
+    hash = state(a)
+
+# Part 1
+print(count)
+
+# Part 2
+print(count - hist.index(hash))
