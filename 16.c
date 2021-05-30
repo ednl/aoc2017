7b4c790f3d8607ca78c56c9ad38326b701a6cb33
E. Dronkert <ednl@users.noreply.github.com>
2021-05-30 13:28:03 +0200

    Ok so the permutation wasn't linear (cyclic)

--- 16.c
+++ 16.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #define A ('a')
 #define N (16)
 #define M (N - 1)
 #define L (N + 1)
+#define CACHESIZE (100)
+
 static uint8_t line[L] = {0};
 
-static void spin(uint8_t *a, uint8_t size, uint8_t count)
+static void spin(uint8_t count)
 {
-    if (count > size - 1) {
-        count %= size;
-    }
-    if (count <= 0) {
-        return;
-    }
     while (count--) {
-        uint8_t t = a[size - 1];
-        for (uint8_t i = size - 1; i > 0; --i) {
-            a[i] = a[i - 1];
+        uint8_t t = line[M];
+        for (uint8_t i = M; i > 0; --i) {
+            line[i] = line[i - 1];
         }
-        a[0] = t;
+        line[0] = t;
     }
 }
 
 static void exchange(uint8_t i, uint8_t j)
 {
-    if (i > M || j > M) {
-        return;
-    }
     uint8_t t = line[i];
     line[i] = line[j];
     line[j] = t;
@@ -36,9 +30,6 @@ static void exchange(uint8_t i, uint8_t j)
 
 static void partner(uint8_t a, uint8_t b)
 {
-    if (a < A || a > A + M || b < A || b > A + M) {
-        return;
-    }
     for (uint8_t i = 0; i < N; ++i) {
         if (line[i] == a) {
             line[i] = b;
@@ -48,50 +39,76 @@ static void partner(uint8_t a, uint8_t b)
     }
 }
 
-int main(void)
+void part1(FILE *fp)
 {
-    for (uint8_t i = 0; i < N; ++i) {
-        line[i] = A + i;
-    }
-
-    // Part 1
-    FILE *fp = fopen("16.txt", "r");
-    if (fp != NULL) {
-        int c;
-        uint8_t i, j;
-        while ((c = fgetc(fp)) != EOF) {
-            if (c == 's') {
-                if (fscanf(fp, "%hhu", &i) == 1) {
-                    spin(line, N, i);
-                }
-            } else if (c == 'x') {
-                if (fscanf(fp, "%hhu/%hhu", &i, &j) == 2) {
-                    exchange(i, j);
-                }
-            } else if (c == 'p') {
-                if (fscanf(fp, "%c/%c", &i, &j) == 2) {
-                    partner(i, j);
-                }
+    int c;
+    uint8_t i, j;
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == 's') {
+            if (fscanf(fp, "%hhu", &i) == 1) {
+                spin(i);
+            }
+        } else if (c == 'x') {
+            if (fscanf(fp, "%hhu/%hhu", &i, &j) == 2) {
+                exchange(i, j);
+            }
+        } else if (c == 'p') {
+            if (fscanf(fp, "%c/%c", &i, &j) == 2) {
+                partner(i, j);
             }
         }
-        fclose(fp);
     }
+}
+
+int main(void)
+{
+    uint8_t cache[CACHESIZE][L] = {0};
+    uint32_t loop = 0;
+
+    FILE *fp = fopen("16.txt", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "File not found.");
+        return 1;
+    }
+
+    // Init
+    uint8_t i, j = A;
+    for (i = 0; i < N; ++i) {
+        line[i] = j++;
+    }
+    strcpy((char *)cache[loop++], (char *)line);
+
+    // Part 1
+    //   0: abcdefghijklmnop
+    //   1: cgpfhdnambekjiol
+    part1(fp);
     printf("%s\n", line);
 
     // Part 2
-    uint8_t loop[N][L] = {0};
-    for (uint8_t i = 0; i < N; ++i) {
-        uint8_t org = line[i];
-        if (org != '\0') {
-            uint8_t cur = org, len = 0, t;
-            do {
-                loop[i][len++] = cur;
-                t = cur - A;
-                cur = line[t];
-                line[t] = '\0';
-            } while (cur != org);
-            printf("%u: %s\n", i, loop[i]);
+    //  60: abcdefghijklmnop
+    //  1B: gjmiofcnaehpdlbk
+    while (loop < CACHESIZE) {
+        // Test if original configuration
+        i = 0;
+        j = A;
+        while (line[i++] == j++)
+            ;
+        if (i == L) {
+            // Found the loop
+            break;
+        } else {
+            // Save & next permutation
+            strcpy((char *)cache[loop++], (char *)line);
+            rewind(fp);
+            part1(fp);
         }
     }
+    fclose(fp);
+
+    if (loop == CACHESIZE) {
+        fprintf(stderr, "Cache too small.");
+        return 2;
+    }
+    printf("%s\n", cache[(1000 * 1000 * 1000) % loop]);
     return 0;
 }
