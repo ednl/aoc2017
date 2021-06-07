0cd0cb8849e261a45d21e384014cd00ad393f2c4
Ewoud Dronkert <ewoud@dronkert.net>
2021-06-07 09:52:25 +0200

    bla

--- 24.c
+++ 24.c
@@ -4,42 +4,62 @@
 #include <inttypes.h>
 
 #define N (57)
-static int comp[N] = {0};
+typedef struct comp {
+    uint8_t id, a, b;
+} Comp, *pComp;
+
+static Comp comp[N] = {0};
 
 static int sort(const void *a, const void *b)
 {
-    int d = (*(const int *)a & 0xff00) - (*(const int *)b & 0xff00);
+    const Comp *p = (const Comp *)a;
+    const Comp *q = (const Comp *)b;
+    int d = p->a - q->a;
     if (d == 0) {
-        return (*(const int *)a & 0xff) - (*(const int *)b & 0xff);
+        d = p->b - q->b;
+        if (d == 0) {
+            return p->id - q->id;
+        }
     }
     return d;
 }
 
+static uint8_t load(void)
+{
+    uint8_t i = 0;
+    FILE *f = fopen("24.txt", "r");
+    if (f != NULL) {
+        uint8_t a, b;
+        while (i < N && fscanf(f, "%"SCNd8"/%"SCNd8" ", &a, &b) == 2) {
+            comp[i] = a <= b ? (Comp){i, a, b} : (Comp){i, b, a};
+            ++i;
+        }
+        fclose(f);
+    }
+    return i;
+}
+
 int main(void)
 {
-    int a, b, n = 0;
-
-    FILE *f = fopen("24.txt", "r");
-    while (n < N && fscanf(f, "%d/%d ", &a, &b) == 2) {
-        comp[n++] = a <= b ? (a << 8) | b : (b << 8) | a;
-    }
-    fclose(f);
-
+    uint8_t n = load();
     qsort(comp, N, sizeof *comp, sort);
     for (int i = 0; i < n; ++i) {
-        a = (comp[i] >> 8) & 0xff;
-        b = comp[i] & 0xff;
-        printf("%2d: %2d %2d\n", i, a, b);
+        printf("%2d: %2d %2d\n", comp[i].id, comp[i].a, comp[i].b);
     }
 
-    int i = 0, port = 0, bridge = 0;
-    // while (i < n) {
-    //     while (comp[i] < port && i < n) {
-    //         ++i;
-    //     }
-    //     if (i < n) {
-    //         bridge +=
-    //     }
-    // }
+    int i = 0, bridge = 0;
+    uint8_t port = 0;
+    while (i < n) {
+        while (i < n && comp[i].a < port) {
+            ++i;
+        }
+        if (i < n) {
+            printf("%d-%d ", comp[i].a, comp[i].b);
+            port = comp[i].b;
+            bridge += port;
+            ++i;
+        }
+    }
+    printf("= %d\n", bridge);
     return 0;
 }
