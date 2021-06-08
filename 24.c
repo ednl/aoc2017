801d459a43d6bc67d13817956efbbe82c6750bed
Ewoud Dronkert <ewoud@dronkert.net>
2021-06-08 19:10:46 +0200

    blah

--- 24.c
+++ 24.c
@@ -47,40 +47,41 @@ int main(void)
         printf("%2d: %2d %2d\n", (comp[i].id = (uint8_t)i), comp[i].a, comp[i].b);
     }
 
-    struct save {
+    struct stackitem {
         int index;
-        int partialsum;
+        int bridge;
     };
-    struct save stack[100] = {0};
+    struct stackitem stack[100] = {0};
     int stacklen = 0;
 
-    int i = 0, bridge = 0;
+    int i = 0, b = 0;
     while (stacklen > 0) {
         --stacklen;
         i = stack[stacklen].index;
-        bridge = stack[stacklen].partialsum;
-        bridge += comp[i].a + comp[i].b;
+        b = stack[stacklen].bridge;
+        b += comp[i].a + comp[i].b;
         int j = i + 1;
         while (j < n && comp[j].a < comp[i].b) {
             ++j;
         }
         while (j < n && comp[j].a == comp[i].b) {
-            ;
+            stack[stacklen++] = (struct stackitem){ .index = j, .bridge = b };
         }
     }
 
-    uint8_t port = 0;
-    while (i < n) {
-        while (i < n && comp[i].a < port) {
-            ++i;
-        }
-        if (i < n) {
-            printf("%d-%d ", comp[i].a, comp[i].b);
-            port = comp[i].b;
-            bridge += port;
-            ++i;
-        }
-    }
-    printf("= %d\n", bridge);
+    // uint8_t port = 0;
+    // int bridge = 0;
+    // while (i < n) {
+    //     while (i < n && comp[i].a < port) {
+    //         ++i;
+    //     }
+    //     if (i < n) {
+    //         printf("%d-%d ", comp[i].a, comp[i].b);
+    //         port = comp[i].b;
+    //         bridge += port;
+    //         ++i;
+    //     }
+    // }
+    // printf("= %d\n", bridge);
     return 0;
 }
