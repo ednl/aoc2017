cc67b22dbfc1783fc7f0eb69b701d46211dce828
Ewoud Dronkert <ewoud@dronkert.net>
2021-06-12 17:41:16 +0200

    day 21 but not very smart

--- 21.py
+++ 21.py
@@ -1,6 +1,11 @@
 import numpy as np
+from math import sqrt
 
-image = '.#./..#/###'
+def str2bit(s):
+    return s.strip().replace('/', '').replace('.', '0').replace('#', '1')
+
+def str2val(s):
+    return int(str2bit(s), 2)
 
 def str2arr(s):
     return (np.array([list(row) for row in s.strip().split('/')]) == '#').astype(np.uint8)
@@ -8,78 +13,76 @@ def str2arr(s):
 def arr2val(a):
     return int(''.join(map(str, a.flatten())), 2)
 
-def str2val(s):
-    return int(s.strip().replace('/', '').replace('.', '0').replace('#', '1'), 2)
-
-def val2arr(dim, i):
-    s = bin(i)[2:]
-    t = '0' * (dim * dim - len(s)) + s
-    return np.array(list(map(int, list(t)))).reshape((dim, dim))
-
-def perm(a):
+def permvals(a):
     p = set()
-    b = np.rot90(a, 1)
     p.add(arr2val(a))
-    p.add(arr2val(b))
-    p.add(arr2val(np.rot90(a, 2)))
-    p.add(arr2val(np.rot90(a, 3)))
-    p.add(arr2val(np.fliplr(a)))
-    p.add(arr2val(np.flipud(a)))
-    p.add(arr2val(np.fliplr(b)))
-    p.add(arr2val(np.flipud(b)))
+    a = a.T
+    p.add(arr2val(a))
+    a = np.flipud(a)
+    p.add(arr2val(a))
+    a = a.T
+    p.add(arr2val(a))
+    a = np.flipud(a)
+    p.add(arr2val(a))
+    a = a.T
+    p.add(arr2val(a))
+    a = np.flipud(a)
+    p.add(arr2val(a))
+    a = a.T
+    p.add(arr2val(a))
     return list(p)
 
-def divide(a):
-    p = []
-    d = a.shape[0]
-    if d % 2 == 0:
-        for i in range(0, d, 2):
-            for j in range(0, d, 2):
-                p.append(arr2val(a[i:i+2,j:j+2]))
-        return (2, p)
-    elif d % 3 == 0:
-        for i in range(0, d, 3):
-            for j in range(0, d, 3):
-                p.append(arr2val(a[i:i+3,j:j+3]))
-        return (3, p)
-    return (0, p)
-
-rule = {}
+rule = { 2: {}, 3: {} }
 with open('21.txt') as f:
     for line in f:
         a, b = line.split(' => ')
-        p = perm(str2arr(a))  # pattern
-        r = str2val(b)        # replacement
-        for i in p:
-            rule[(2 if len(a) == 5 else 3, i)] = r
+        i = 2 if len(a) == 5 else 3
+        p = permvals(str2arr(a))  # search patterns
+        r = str2val(b)            # replacement
+        for j in p:
+            rule[i][j] = r
 
-print(str2val(image))
-print(rule)
+def partition(bitstr, area, size, chunk):
+    step = size * chunk  # index step of 2 or 3 rows
+    p = []
+    for i in range(0, area, step):   # row index per 2 or 3 rows
+        for j in range(0, size, chunk):  # col index per 2 or 3 cols
+            s = ''
+            for k in range(0, step, size):  # extra row index for 2 or 3 conseq. rows
+                n = i + j + k
+                s += bitstr[n:n + chunk]
+            p.append(int(s, 2))
+    return p
 
-# org: ab
-#      cd
-# rot1: ca  rot2: dc  rot3: bd
-#       db        ba        ac
-# flp1: ba  flp2: cd
-#       dc        ab
-# r+f1: ac  r+f2: db
-#       bd        ca
-#
-# abcd, acbd, dcba, dbca
-# badc, bdac, cdab, cadb
+def val2bit(val, partarea):
+    s = bin(val)[2:]
+    return '0' * (partarea - len(s)) + s
 
-# org: abc
-#      def
-#      ghi
-# rot1: gda  rot2: ihg  rot3: cfi
-#       heb        fed        beh
-#       ifc        cba        adg
-# flp1: cba  flp2: ghi
-#       fed        def
-#       ihg        abc
-# r+f1: adg  r+f2: ifc
-#       beh        heb
-#       cfi        gda
-#
-# abcdefghi, adgbehcfi, ihgfedcba, ifchebgda
-# cbafedihg, cfibehadg, ghidefabc, gdahebifc
+def evolve(bitstr):
+    area = len(bitstr)
+    size = int(sqrt(area))
+    chunk = 2 if size % 2 == 0 else 3
+    parts = partition(bitstr, area, size, chunk)
+    transform = list(map(lambda x: rule[chunk][x], parts))
+    partperrow = size // chunk
+    parts = partperrow * partperrow
+    chunk += 1
+    size = partperrow * chunk
+    area = size * size
+    partarea = chunk * chunk
+    a = list(map(lambda x: val2bit(x, partarea), transform))
+    s = ''
+    for i in range(0, parts, partperrow):
+        for k in range(0, partarea, chunk):
+            for j in range(partperrow):
+                s += a[i + j][k:k + chunk]
+    return s
+
+image = '.#./..#/###'
+art = str2bit(image)
+print(0, sum(map(int, list(art))))
+for i in range(1, 19):
+    art = evolve(art)
+    print(i, sum(map(int, list(art))))
+# part 1: 179
+# part 2: 2766750
